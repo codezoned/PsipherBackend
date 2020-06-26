e6a10f65079464e2c9e2715c679c1ad2c1dec146
Rahul Krishnan <rahulkrishnan221@gmail.com>
2020-06-26 21:00:07 +0530

    Initial SetUp for SpringBoot Application with maven

---
+++ src/main/java/com/psipher/application/PsipherApplication.java
@@ -0,0 +1,13 @@
+package com.psipher.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsipherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsipherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psipher/application/PsipherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psipher.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsipherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
