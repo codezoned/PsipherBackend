8dc5e28db1505085240b086620f871b2cf8207ef
Rahul Krishnan <rahulkrishnan221@gmail.com>
2020-06-28 00:38:54 +0530

    Setup for making compatible with SAM, lambda and API gateway

--- src/main/java/com/psipher/application/PsipherApplication.java
+++ src/main/java/com/psipher/application/PsipherApplication.java
@@ -2,9 +2,10 @@ package com.psipher.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PsipherApplication {
+public class PsipherApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PsipherApplication.class, args);
---
+++ src/main/java/com/psipher/application/model/User.java
@@ -0,0 +1,23 @@
+package com.psipher.application.model;
+
+public class User {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String id;
+    private String name;
+
+}
